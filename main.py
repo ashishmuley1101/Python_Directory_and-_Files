494b767614263203d720960e43fa78de13a87630
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-07 19:52:21 +0530

    Changing Directory in Python.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 
-# Get Current Directory in Python using getcwd() method
+# Changing Directory in Python
 
-# The present working directory using the getcwd() method of the os module.
-# This method returns the current working directory in the form of a string.
+# changing the current working directory by using the chdir() method.
+# The new path that we want to change into must be supplied as a string to this method.
+# And we can use both the forward-slash / or the backward-slash \ to separate the path elements
 
-# Syntax : os.getcwd()
+# Syntax : os.chdir("new_dir_path")
 
 import os
 
 current_dir = os.getcwd()
-print(current_dir)
+print(current_dir)  # Output : C:\work\python\Python\pythonDirectoryFiles
 
-# Output : C:\work\python\Python\pythonDirectoryFiles
+os.chdir("C:\work\python\Python")
+print(os.getcwd())  # Output : C:\work\python\Python
+
