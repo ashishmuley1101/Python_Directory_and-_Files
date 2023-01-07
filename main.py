24639620baaf5fdddc9a23bc839aabf07766c7e5
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-07 19:40:35 +0530

    Get Current Directory in Python using getcwd() method.

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Get Current Directory in Python using getcwd() method
+
+# The present working directory using the getcwd() method of the os module.
+# This method returns the current working directory in the form of a string.
+
+# Syntax : os.getcwd()
+
+import os
+
+current_dir = os.getcwd()
+print(current_dir)
+
+# Output : C:\work\python\Python\pythonDirectoryFiles
